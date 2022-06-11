3c9e8d8edb718cdb637f6387084f867de886b88f
goodgamersz665@gmail.com <goodgamersz665@gmail.com>
2022-06-11 19:34:27 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  async function load() {
+    let res = await fetch("jsonplaceholder.typicode.com/users/")
+    let users = await res.json()
+    
+    if(res.ok) {
+      return users
+    }
+  }
+  
+  let promise = load()
+</script>
+
+{#await promise}
+<p>Loading...</p>
+{:then user}
+<p>{user.name}</p>
+{:catch err}
+<p>{catch}</p>
+{/await}
