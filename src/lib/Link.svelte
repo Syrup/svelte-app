ab1b9b8b506f85ba443cda7d718818d768c63cf8
goodgamersz665@gmail.com <goodgamersz665@gmail.com>
2022-06-11 21:30:54 +0700

    v

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,6 +1,8 @@
 <script>
   export { className as class, to, variant };
   let className = ""
+  let to;
+  let variant = "primary"
 </script>
 
 <a href={to.startsWith("/") ? to : to.startsWith("https://") ? to : to.startsWith("http://") ? to : `/${to}` } target="_blank" class={`link link-${variant} ${className}`}><slot></slot></a>
