ce2d440f373a187ff93869157a76a416072f6daf
goodgamersz665@gmail.com <goodgamersz665@gmail.com>
2022-06-14 02:50:00 +0700

    j

--- src/lib/Github.svelte
+++ src/lib/Github.svelte
@@ -16,21 +16,23 @@
   } from "sveltestrap"
 </script>
 
-<Row cols={2}>
-{#each repos as repo}
-<Col>
-  <Card color="light" class="mb-3">
-    <CardHeader>
-      <CardTitle>{repo.name}</CardTitle>
-    </CardHeader>
-    <CardBody>
-      <CardSubtitle class="text-muted">by {@html repo.authors.map(a => `<a href=${a.link} target="_blank" class="link-seocndary" style="text-decoration: none;">${a.username}</a>`).join(", ")}</CardSubtitle>
-      <hr />
-      <CardText>{repo.text}</CardText>
-      <a href={repo.repo} class="btn btn-outline-secondary"><Icon name="box-arrow-up-right" /> Go to repo</a>
-    </CardBody>
-    <CardFooter class="text-muted">Copyright &copy; 2022 Syrup</CardFooter>
-  </Card>
-</Col>
-{/each}
-</Row>
+<Container fluid>
+  <Row cols={2}>
+  {#each repos as repo}
+  <Col>
+    <Card color="light" class="mb-3">
+      <CardHeader>
+        <CardTitle>{repo.name}</CardTitle>
+      </CardHeader>
+      <CardBody>
+        <CardSubtitle class="text-muted">by {@html repo.authors.map(a => `<a href=${a.link} target="_blank" class="link-seocndary" style="text-decoration: none;">${a.username}</a>`).join(", ")}</CardSubtitle>
+        <hr />
+        <CardText>{repo.text}</CardText>
+        <a href={repo.repo} class="btn btn-outline-secondary"><Icon name="box-arrow-up-right" /> Go to repo</a>
+      </CardBody>
+      <CardFooter class="text-muted">Copyright &copy; 2022 Syrup</CardFooter>
+    </Card>
+  </Col>
+  {/each}
+  </Row>
+</Container>
