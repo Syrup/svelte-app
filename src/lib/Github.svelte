c07c95569f4c2353ecb179a6051e802e5a266bc4
goodgamersz665@gmail.com <goodgamersz665@gmail.com>
2022-06-14 02:09:58 +0700

    component

---
+++ src/lib/Github.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import repos from "../../static/data/repos.json"
+  import {
+    Styles,
+    Container,
+    Card,
+    CardBody,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    CardFooter,
+    Icon
+  } from "sveltestrap"
+</script>
+
+{#each repos as repo}
+<Card color="light" class="mb-3">
+  <CardHeader>
+    <CardTitle>{repo.name}</CardTitle>
+  </CardHeader>
+  <CardBody>
+    <CardSubtitle class="text-muted">by {@html repo.authors.map(a => `<a href=${a.link} target="_blank">${a.username}</a>`).join(", ")}</CardSubtitle>
+    <hr />
+    <CardText>{repo.text}</CardText>
+    <a href={repo.repo} class="btn btn-outline-secondary"><Icon name="box-arrow-up-right" /> Go to repo</a>
+  </CardBody>
+  <CardFooter class="text-muted">Copyright &copy; 2022 Syrup</CardFooter>
+</Card>
+{/each}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,24 +2,21 @@
   // import 'bulma/css/bulma.css'
   // import '@fortawesome/fontawesome-free/css/all.css'
   import Link from "$lib/Link.svelte";
+  import Github from "$lib/Github.svelte"
   // import Markdoc from "$lib/Markdoc.svelte";
   // import { Route, Router } from "svelte-navigator"
   import {
     Styles,
+    Button,
     Container,
     Card,
-    CardBody,
-    CardHeader,
-    CardSubtitle,
-    CardText,
-    CardTitle,
-    CardFooter,
+    Fade,
     Icon
   } from "sveltestrap"
   import Nav from "$lib/Nav.svelte"
-  import repos from "../../static/data/repos.json"
-  
   // const file = fs.readFileSync("")
+  
+  let isGithubOpen = false;
   
   async function load() {
     let res = await fetch("https://jsonplaceholder.typicode.com/users/")
@@ -47,18 +44,12 @@
 <h1>Home</h1>
 <br />
 
-{#each repos as repo}
-<Card color="light" class="mb-3">
-  <CardHeader>
-    <CardTitle>{repo.name}</CardTitle>
-  </CardHeader>
-  <CardBody>
-    <CardSubtitle class="text-muted">by {@html repo.authors.map(a => `<a href=${a.link} target="_blank">${a.username}</a>`).join(", ")}</CardSubtitle>
-    <CardText>{repo.text}</CardText>
-    <a href={repo.repo} class="btn btn-outline-secondary"><Icon name="box-arrow-up-right" /> Go to repo</a>
-  </CardBody>
-  <CardFooter class="text-muted">Copyright &copy; 2022 Syrup</CardFooter>
+<Card body>
+  
+  <Button on:click={toggleGithub}><Icon name="github" /> Github</Button>
+  <Fade bind:isOpen={isGithubOpen}>
+    <Github />
+  </Fade>
 </Card>
-{/each}
 
 </Container>
